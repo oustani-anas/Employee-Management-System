3c6dd20b97e50c5beaa99d667bc0fe4bbd4f6a89
oustani-anas <anas.migos20@gmail.com>
2024-06-28 23:20:13 +0100

    adding controller on the backend

--- EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/Program.cs
@@ -1,7 +1,9 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -35,6 +37,13 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+
+app.MapGet("/test", () => {
+    Console.WriteLine("Hello, World!");
+    return ("okaaaaaaaay");
+});
+
 
 app.Run();
 
